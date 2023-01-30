60609b0e540e13e1abe06e952e1d8082eb46cbd1
nateraluis <luis.natera@systemiq.earth>
2023-01-30 10:38:11 +0100

    Config files fro NeoVIm starter

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,5 @@
+vim.opt.relativenumber = true
+
+vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('i', 'jj', '<ESC>', { noremap = true })
+
